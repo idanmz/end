0628317f691052b1d53bb15aaaab78afee83dbef
Saida Namazova <114984989+idanmz@users.noreply.github.com>
2023-01-08 16:55:48 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    var element = document.getElementById("myDIV");
+    var list = document.getElementById("sect");
+   element.classList.toggle("murad");
+   list.classList.toggle("sect")
+}   
